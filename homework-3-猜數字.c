0f7e87fb5b2c6dc3428f469c78352afd6726a28e
cubeman0963 <42257833+cubeman0963@users.noreply.github.com>
2018-08-10 15:56:41 +0800

    Add files via upload

---
+++ homework-3-猜數字.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int guess,answer;
+    answer = 67;
+    printf("猜一個數字: ");
+    while(guess!=answer){
+        scanf("%d", &guess);
+        if(guess>answer){
+            printf("太大了,在猜一次!\n");
+            continue;
+        }
+        if(guess<answer){
+            printf("太小了,在猜一次!\n");
+            continue;
+        }
+    }
+    printf("猜對了,恭喜!\n");
+    return 0;
+}
